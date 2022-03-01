9f3582dab21c3d30ced685e8a4bd82dc0ee8d213
HotrodMaster <83735213+HotrodMaster@users.noreply.github.com>
2022-03-01 17:34:15 +0200

    Delete remake_all.sh

--- remake_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-bash uninstall_packages_group.sh && \
-echo "Removing *pkg.tar.zst files"; rm -f *.pkg.tar.zst && \
-echo "Generating PKGBUILD..."; ./build.sh && \
-echo "Invoking makepkg..."; makepkg && \
-echo "Updating local repository..."; bash update-local-repo.sh 1>/dev/null
